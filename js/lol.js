7151ab234323675d8ca46809a1d88f33267038d7
edjuarezp <edjuarezp@L01452763M01.local>
2026-02-16 11:51:02 -0600

    Reaccionar al evento onclick con js

--- js/lol.js
+++ js/lol.js
@@ -1,2 +1,9 @@
 //document es el pretotipo que contiene el DOM (Document Object Model)
-console.log(document);
+//console.log(document);
+
+const div_gwen = document.getElementById("gwen");
+console.log(div_gwen);
+
+div_gwen.onclick = () => {
+    console.log("click en gwen");
+}
