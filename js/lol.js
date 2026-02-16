c707a5fbb025a3f82dfbbb0a0beb57b2b190a8e6
edjuarezp <edjuarezp@L01452763M01.local>
2026-02-16 12:05:22 -0600

    crear objeto gwen y acceder a sus atributos por medio de la notación

--- js/lol.js
+++ js/lol.js
@@ -1,18 +1,24 @@
 //document es el pretotipo que contiene el DOM (Document Object Model)
 //console.log(document);
+const gwen = {
+    nombre: "Gwen",
+    descripcion: `Gwen, una antigua muñeca que se transformó y cobró vida a través de la magia, 
+        usa las mismas herramientas que en su momento la crearon. 
+        Lleva el peso del amor de su creadora a cada paso, sin dar nada por sentado. 
+        Bajo su mando está la Niebla Sagrada, una magia antigua y protectora que bendijo las tijeras, 
+        las agujas y el hilo de coser de Gwen. Muchas cosas son nuevas para Gwen, 
+        pero sigue decidida a luchar con gozo por el bien que prevalece en un mundo roto.`,
+    tipo: "mago",
+    imagen: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Gwen_0.jpg",
+}
 
 const div_gwen = document.getElementById("gwen");
 console.log(div_gwen);
 
 div_gwen.onclick = () => {
     div_gwen.innerHTML = `
-        <p class="is-size-2">Gwen</p>
-        <p>Gwen, una antigua muñeca que se transformó y cobró vida a través de la magia, 
-        usa las mismas herramientas que en su momento la crearon. 
-        Lleva el peso del amor de su creadora a cada paso, sin dar nada por sentado. 
-        Bajo su mando está la Niebla Sagrada, una magia antigua y protectora que bendijo las tijeras, 
-        las agujas y el hilo de coser de Gwen. Muchas cosas son nuevas para Gwen, 
-        pero sigue decidida a luchar con gozo por el bien que prevalece en un mundo roto.</p>
-        <span class="tag">mago</span>
+        <p class="is-size-2">${gwen.nombre}</p>
+        <p>${gwen.descripcion}</p>
+        <span class="tag">${gwen.tipo}</span>
     `;
 }
