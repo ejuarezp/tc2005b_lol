7ffb32577aecba35b0a91a233cb3f89a405b37aa
edjuarezp <edjuarezp@L01452763M01.local>
2026-02-16 12:28:06 -0600

    agregar modal de Mordekaiser

--- js/lol.js
+++ js/lol.js
@@ -12,6 +12,16 @@ const gwen = {
     imagen: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Gwen_0.jpg",
 }
 
+const mordekaiser = {
+    nombre: "Mordekaiser",
+    descripcion: `Mordekaiser es un señor de la guerra nigromante que domina el carril superior con 
+        daño mágico sostenido, gran aguante y una definitiva que aísla a enemigos en su "reino de la muerte". 
+        Destaca por su pasiva de daño en área, su maza Ocaso y su capacidad para robar estadísticas, 
+        siendo popular por su contundencia.`,
+    tipo: "tanque",
+    imagen: "https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Mordekaiser_0.jpg",
+}
+
 const div_gwen = document.getElementById("gwen");
 console.log(div_gwen);
 
@@ -36,3 +46,52 @@ const mostrar_imagen = () => {
 mostrar_imagen();
 
 div_gwen.onclick = mostrar_datos;
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Functions to open and close a modal
+  function openModal($el) {
+    $el.classList.add('is-active');
+  }
+
+  function closeModal($el) {
+    $el.classList.remove('is-active');
+  }
+
+  function closeAllModals() {
+    (document.querySelectorAll('.modal') || []).forEach(($modal) => {
+      closeModal($modal);
+    });
+  }
+
+  // Add a click event on buttons to open a specific modal
+  (document.querySelectorAll('.js-modal-trigger') || []).forEach(($trigger) => {
+    const modal = $trigger.dataset.target;
+    const $target = document.getElementById(modal);
+
+    $trigger.addEventListener('click', () => {
+      openModal($target);
+    });
+  });
+
+  // Add a click event on various child elements to close the parent modal
+  (document.querySelectorAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button') || []).forEach(($close) => {
+    const $target = $close.closest('.modal');
+
+    $close.addEventListener('click', () => {
+      closeModal($target);
+    });
+  });
+
+  // Add a keyboard event to close all modals
+  document.addEventListener('keydown', (event) => {
+    if(event.key === "Escape") {
+      closeAllModals();
+    }
+  });
+});
+
+document.getElementById("contenido_modal").innerHTML = `
+    <p class="is-size-2">${mordekaiser.nombre}</p>
+    <p>${mordekaiser.descripcion}</p>
+    <span class="tag">${mordekaiser.tipo}</span>
+`;
