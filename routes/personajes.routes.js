34027a3c94e2f99efa47358b2336a1d408fb1a9d
edjuarezp <edjuarezp@L01452763M01.local>
2026-03-10 12:23:38 -0600

    proteger rutas con permiso de ver personajes

--- routes/personajes.routes.js
+++ routes/personajes.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const isAuth = require('../util/is-auth');
 const canCreate = require('../util/can-create');
+const canView = require('../util/can-view');
 const personajesController = require('../controllers/personajes.controller');
 
 router.get('/new', isAuth, canCreate, personajesController.get_add);
@@ -10,7 +11,7 @@ router.get('/add', isAuth, canCreate, personajesController.get_add);
 router.get('/nuevo', isAuth, canCreate, personajesController.get_add);
 router.post('/new', isAuth, canCreate, personajesController.post_add);
 router.get('/old', isAuth, personajesController.get_old);
-router.use('/:personaje_id', isAuth, personajesController.get_list);
-router.use(isAuth, personajesController.get_list);
+router.use('/:personaje_id', isAuth, canView, personajesController.get_list);
+router.use(isAuth, canView, personajesController.get_list);
 
 module.exports = router;
